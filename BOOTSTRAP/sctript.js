058481fc418f5528dceba53548b3ad8954e749b6
laksh305113-tech <laksh305113@gmail.com>
2026-05-31 18:04:44 +0530

    Create sctript.js

---
+++ BOOTSTRAP/sctript.js
@@ -0,0 +1,9 @@
+var a = 5 ;
+var b  = 10;
+console.log(a+b * 19) ;
+console.log(typeof a , typeof b);
+var  c = "helllo world " ;
+console.log(typeof c) ;
+
+console.log("Hello I'm the Boss") ; 
+    
